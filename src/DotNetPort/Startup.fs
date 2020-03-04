c922291d1375045a5ec65dd9fa4c5d69f52bf1c1
wk <somnuk.wk@gmail.com>
2020-03-04 18:18:19 +0700

    Port

--- src/DotNetPort/Startup.fs
+++ src/DotNetPort/Startup.fs
@@ -6,15 +6,16 @@ open System.Linq
 open System.Threading.Tasks
 open Microsoft.AspNetCore.Builder
 open Microsoft.AspNetCore.Hosting
-open Microsoft.AspNetCore.HttpsPolicy;
+open Microsoft.AspNetCore.HttpsPolicy
 open Microsoft.AspNetCore.Mvc
 open Microsoft.Extensions.Configuration
 open Microsoft.Extensions.DependencyInjection
 
 type Startup private () =
-    new (configuration: IConfiguration) as this =
-        Startup() then
-        this.Configuration <- configuration
+
+    new(configuration: IConfiguration) as this =
+        Startup()
+        then this.Configuration <- configuration
 
     // This method gets called by the runtime. Use this method to add services to the container.
     member this.ConfigureServices(services: IServiceCollection) =
@@ -32,4 +33,4 @@ type Startup private () =
         app.UseHttpsRedirection() |> ignore
         app.UseMvc() |> ignore
 
-    member val Configuration : IConfiguration = null with get, set
+    member val Configuration: IConfiguration = null with get, set
